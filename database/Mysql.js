ade8cac8c55d83d5e9e729e679977430ba224adb
damasac <chanpan.nuttaphon@gmail.com>
2017-12-14 13:07:43 +0700

    mysql

---
+++ database/Mysql.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Chanpan!((#',
+    database: 'demo'
+});
+
+exports.User = function() {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM tb_users', function (error, results, fields) {
+            if (error) reject(error);
+            resolve(results);
+        });
+    });
+}
