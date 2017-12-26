0188b15d28d8668b876d4c9bcf07c924999ec6d9
chanpan <chanpan.nuttaphon@gmail.com>
2017-12-26 11:28:26 +0700

    readonly form

--- settings/setting.js
+++ settings/setting.js
@@ -1,4 +1,5 @@
-let config = require("./database/Mysql");
+var config = require("./database/Mysql");
+
 $("#btnConnectDatabase").click(function(){
    let select_db = $("#select-db").val();
    let host_name = $("#host-name").val();
@@ -7,13 +8,30 @@ $("#btnConnectDatabase").click(function(){
    let port = $("#port").val();
    let dbname = $("#dbname").val();
    
-   if(select_db != 1){
+   if(select_db != 1 && select_db != 2){ //!= mysql and != sqlite 
        alert("กรุณาเลือกประเภทฐานข้อมูล");return false;
    }
-   let data = {host:host_name, username:username, password:password, port:port, dbname:dbname};
-   config.ConnectDb(data).then((pool)=>{
-        console.log('success');
-   });
-
+   if(select_db == 1){
+       //mysql
+       let data = {host:host_name, username:username, password:password, port:port, dbname:dbname};
+        config.ConnectDb(data).then((pool)=>{
+                console.log('success');
+        });
+   }
+ 
    return false;
 });
+
+$("#select-db").change(function(){
+    if($(this).val() == 1){
+        EnableForm();
+    }else{
+        DisableForm();
+    }
+});
+function DisableForm(){
+    $('#frm-setting input').prop('readonly', true);
+}DisableForm();
+function EnableForm(){
+    $('#frm-setting input').prop('readonly', false);
+}
